ed504260e00d371c61684fc5011b8b9a550d3998
GitaekLee <coolfamily77@gmail.com>
2020-02-06 00:14:11 -0800

    Basic Go lang Add
    - Interface
    - Concurrency
    - Go routine
    - About Channel

--- world.go
+++ world.go
@@ -485,6 +485,15 @@ func main() {
 	go alphabet()
 	time.Sleep(3000 * time.Millisecond)
 	fmt.Println("main terminated")
+
+	/*
+
+		What are channels
+
+		Channels can be thought as pipes using which Goroutines communicate.
+		Similar to how water flows from one end to another in a pipe,
+		data can be sent from one end and received from the another end using channels.
+	*/
 }
 
 func numbers() {
